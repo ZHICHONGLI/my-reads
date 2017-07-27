b1ad8417127b42996c495c61712076bd770db3de
Zhichong Li <leezhc@gmail.com>
2017-07-26 22:50:12 -0700

    feature: revised BookComponent as stateless component

--- src/components/BookComponent.js
+++ src/components/BookComponent.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BookComponent extends Component {
-  constructor(props) {
-    super(props)
-  };
-  onChange(event) {
-    this.props.handleChange(this.props.book, event.target.value);
-  };
-  render() {
-    const {book} = this.props; 
+  const BookComponent = ({book, handleChange}) => {
+    const onChange = event => {
+      handleChange(book, event.target.value);
+    };
     return (book &&
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: `url(${book?book.imageLinks.smallThumbnail:null})`}}></div>
-          
+          <div className="book-cover"
+               style={{ width: 128, height: 174, backgroundImage: `url(${book?book.imageLinks.smallThumbnail:null})`}}>
+          </div>
           <div className="book-shelf-changer">
-            <select value={book.shelf} onChange={this.onChange.bind(this)}>
+            <select value={book.shelf} onChange={onChange}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -30,9 +26,8 @@ class BookComponent extends Component {
               <div className="book-authors" key={author}>{author}</div>
             ))
           }
-        </div>
+      </div>
     );
   }
-}
 
 export default BookComponent;
